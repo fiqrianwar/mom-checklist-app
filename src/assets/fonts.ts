b63c1e64de74fadfaea374e3c6cd09e2ee020b5a
fiqrianwar <fiqrianwar01@gmail.com>
2026-07-17 07:09:31 +0700

    feat(setup): assets config

---
+++ src/assets/fonts.ts
@@ -0,0 +1,5 @@
+export const fonts = {
+  'Satoshi-Regular': require('./fonts/Satoshi-Regular.ttf'),
+  'Satoshi-Medium': require('./fonts/Satoshi-Medium.ttf'),
+  'Satoshi-Bold': require('./fonts/Satoshi-Bold.ttf'),
+};
---
+++ src/assets/icons.ts
@@ -0,0 +1,7 @@
+export const icons = {
+  close: require('./icons/close.png'),
+  mom: require('./icons/mom.png'),
+  rightArrow: require('./icons/right-arrow.png'),
+  bag: require('./icons/bag.png'),
+  leftArrow: require('./icons/arrow-left.png'),
+};
